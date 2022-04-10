f94199cd0883ceca8fedebbaaa08ebd34612930c
seongyle <seongyle@c4r3s8.42seoul.kr>
2022-04-10 12:34:19 +0900

    atomic design

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import App from './pages/ToDoList/template'
 
 ReactDOM.render(
   <React.StrictMode>
