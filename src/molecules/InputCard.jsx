867e41d81199a74121f751d466cdd7faf6d602d4
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-05-08 14:59:06 +0800

    [IMPROVE] apply prettier

--- src/molecules/InputCard.jsx
+++ src/molecules/InputCard.jsx
@@ -1,28 +1,19 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import DatePicker from "../atoms/DatePicker";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import DatePicker from '../atoms/DatePicker';
 
 const card = (
   <Box>
     <CardContent>
-      <TextField
-        id="title"
-        label="제목을 입력하세요"
-        variant="standard"
-      ></TextField>
+      <TextField id="title" label="제목을 입력하세요" variant="standard"></TextField>
     </CardContent>
     <CardContent>
-      <TextField
-        id="detail"
-        label="상세내용을 입력하세요"
-        variant="standard"
-        multiline="true"
-      ></TextField>
+      <TextField id="detail" label="상세내용을 입력하세요" variant="standard" multiline="true"></TextField>
     </CardContent>
     <CardContent>
       <DatePicker></DatePicker>
--- src/molecules/TaskCard.jsx
+++ src/molecules/TaskCard.jsx
@@ -1,46 +1,33 @@
-import * as React  from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-function card (props){
- 
-  return(
-   
-     <React.Fragment>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }}>{props.content}</Typography>
-      <Typography sx={{ fontSize: 8 }} color="text.secondary">
-        2022.04.30
-      </Typography>
-      <Typography variant="body2">
-        {props.detail}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">상태 변경</Button>
-    </CardActions>
-  </React.Fragment>
+function card(props) {
+  return (
+    <React.Fragment>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }}>{props.content}</Typography>
+        <Typography sx={{ fontSize: 8 }} color="text.secondary">
+          2022.04.30
+        </Typography>
+        <Typography variant="body2">{props.detail}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">상태 변경</Button>
+      </CardActions>
+    </React.Fragment>
   );
-    
 }
-function OutlinedCard (props){
- 
-
-  return(
-    
+function OutlinedCard(props) {
+  return (
     <Box sx={{ minWidth: 275 }}>
-      <Card variant="outlined">
-       {card(props)}
-      </Card>
+      <Card variant="outlined">{card(props)}</Card>
     </Box>
-  
-  )
-  
+  );
 }
 
-
-export default OutlinedCard
+export default OutlinedCard;
