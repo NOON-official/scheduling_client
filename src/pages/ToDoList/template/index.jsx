82f634430c374355d6f81b9925977e0718018176
YeonSeong-Lee <dltjddus3@gmail.com>
2022-05-11 02:27:22 +0900

    fetch data & 각 상태에 맞는 데이터 array 생성

--- src/pages/ToDoList/template/index.jsx
+++ src/pages/ToDoList/template/index.jsx
@@ -1,7 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Table from '../../../orgainisms/Table';
 import './style.css';
 
 function App() {
+  const [cards, setCards] = useState({ cards: [] });
+  const [ToDo, setToDo] = useState([]);
+  const [Process, setProcess] = useState([]);
+  const [Done, setDone] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/card');
+      await setCards(response.data.data.card);
+      await setToDo(cards.filter((value) => value.state == 0));
+      await setProcess(cards.filter((value) => value.state == 1));
+      await setDone(cards.filter((value) => value.state == 2));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(fetchData, []);
+
   return (
     <div>
       <h1>SCHEDULING</h1>
